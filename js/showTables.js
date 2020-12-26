1f6f8889b6e7c1e5c4d34659daae6c25aa0b521f
=suleman-shoukat <sulemanshoukaat@gmail.com>
2020-12-27 02:16:38 +0500

    responsive issues resolved

--- js/showTables.js
+++ js/showTables.js
@@ -117,57 +117,64 @@ form.addEventListener("submit", (e) => {
     showCancelButton: false,
     showConfirmButton: false,
   });
-  // File or Blob named mountains.jpg
-  var file = Editlogo;
+  if (Editlogo !== null) {
+    // File or Blob named mountains.jpg
+    var file = Editlogo;
 
-  // Upload file and metadata to the object 'images/mountains.jpg'
-  var uploadTask = storageRef.child("Logos/" + file.name).put(file);
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    var uploadTask = storageRef.child("Logos/" + file.name).put(file);
 
-  // Listen for state changes, errors, and completion of the upload.
-  uploadTask.on(
-    firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-    function (snapshot) {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log("Upload is " + progress + "% done");
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log("Upload is paused");
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log("Upload is running");
-          break;
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+      function (snapshot) {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log("Upload is paused");
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log("Upload is running");
+            break;
+        }
+      },
+      function (error) {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/unauthorized":
+            // User doesn't have permission to access the object
+            break;
+
+          case "storage/canceled":
+            // User canceled the upload
+            break;
+
+          case "storage/unknown":
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      function () {
+        // Upload completed successfully, now we can get the download URL
+        uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+          console.log("runing");
+          SelectedRecord.logo = downloadURL;
+          SelectedRecord.date = curday("/");
+
+          database.ref("Data/" + SelectedRecord.u_id).set(SelectedRecord);
+          swal("Success!", "data has been added", "success");
+        });
       }
-    },
-    function (error) {
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case "storage/unauthorized":
-          // User doesn't have permission to access the object
-          break;
+    );
+  } else {
+    SelectedRecord.date = curday("/");
 
-        case "storage/canceled":
-          // User canceled the upload
-          break;
-
-        case "storage/unknown":
-          // Unknown error occurred, inspect error.serverResponse
-          break;
-      }
-    },
-    function () {
-      // Upload completed successfully, now we can get the download URL
-      uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-        console.log("runing");
-        SelectedRecord.logo = downloadURL;
-        SelectedRecord.date = curday("/");
-
-        database.ref("Data/" + SelectedRecord.u_id).set(SelectedRecord);
-        swal("Success!", "data has been added", "success");
-      });
-    }
-  );
+    database.ref("Data/" + SelectedRecord.u_id).set(SelectedRecord);
+    swal("Success!", "data has been added", "success");
+  }
 });
 //current date
 var curday = function (sp) {
